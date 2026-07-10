ebc36932b54c5aff411a38d31de7fa5aa9efb5db
vernea2112 <verneageral@gmail.com>
2026-07-10 19:46:03 +0100

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/luxury-paradise-portals/', // <-- Define a rota correta para o GitHub Pages funcionar
+  base: '/luxury-paradise-portals/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
